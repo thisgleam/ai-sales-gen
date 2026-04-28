ad1464c8bdd5d9ff57da5a6ae051f26ec9655845
Muhammad Gleam Mulyawan <73107276+thisgleam@users.noreply.github.com>
2026-04-28 22:20:04 +0700

    Update init-app.sh

--- railway/init-app.sh
+++ railway/init-app.sh
@@ -2,4 +2,10 @@
 set -eu
 
 php artisan config:clear
+php artisan cache:clear
+php artisan view:clear
+php artisan storage:link
 php artisan migrate --force
+
+# Ensure public/hot is removed so Laravel doesn't look for Vite dev server
+rm -f public/hot
